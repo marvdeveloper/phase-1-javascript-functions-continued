ccaf8583b9bb0563f26e318cb2b33026ab26d3b8
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-03-16 19:47:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // code your solution here
+razzle(); // Outputs: "You've been razzled!"
+
+function razzle() {
+  console.log("You've been razzled!");
+}
+
+const fn = function() {
+    console.log("Yet more razzling");
+  };
+  fn(); // Outputs: "Yet more razzling"
+
+  (function() {
+  })();
+  
+  function outer(greeting) {
+    const msg = "It's a fine day to learn";
+    return function(name) {
+      console.log(`${greeting}, ${name}! ${msg}`);
+    };
+  }
+  
+  const greet = outer("Hello");
+  greet("Student"); // Outputs: "Hello, Student! It's a fine day to learn"
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  console.log(saturdayFun()); // Outputs: "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("go hiking")); // Outputs: "This Saturday, I want to go hiking!"
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+  console.log(mondayWork()); // Outputs: "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // Outputs: "This Monday, I will work from home."
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // Outputs: "You are !!!a dedicated programmer!!!"
+console.log(wrapAdjective("%")("an awesome coder")); // Outputs: "You are %an awesome coder%!"
